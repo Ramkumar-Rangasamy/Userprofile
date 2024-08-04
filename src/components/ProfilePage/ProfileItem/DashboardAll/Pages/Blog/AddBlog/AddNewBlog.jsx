012620862a77700d702e254df16f156bd432469e
Ramkumar-Rock <always.ramkumar7@gmail.com>
2024-08-04 08:50:26 +0530

    this all item in dashbaord

--- src/components/ProfilePage/ProfileItem/DashboardAll/Pages/Blog/AddBlog/AddNewBlog.jsx
+++ src/components/ProfilePage/ProfileItem/DashboardAll/Pages/Blog/AddBlog/AddNewBlog.jsx
@@ -2,8 +2,18 @@ import React, { useRef, useState } from "react";
 import Quill from "quill";
 import BlogInput from "./BlogInput";
 import Editor from "./Editor";
-
+import Blog from '../Blog'
 const AddNewBlog = () => {
+  const [showAddNewBlog, setShowAddNewBlog] = useState(false); 
+  
+  const handleAddClick = () => {
+    setShowAddNewBlog(true); 
+  };
+
+  const handleCancel = () => {
+    setShowAddNewBlog(false); 
+  };
+
   const [iscurrentFocus, setIscurrentFocus] = useState(null);
   const [newBlogdata, setNewBlogdata] = useState({
     title: "",
@@ -49,160 +59,167 @@ const AddNewBlog = () => {
   };
 
   return (
-    <div className="create-blog-cnt">
-      <div className="blog-details-cnt">
-        <BlogInput
-          currentFocus={iscurrentFocus}
-          setcurrentFocus={(label) => setIscurrentFocus(label)}
-          updateValue={(value) =>
-            setNewBlogdata({ ...newBlogdata, title: value })
-          }
-          value={newBlogdata.title}
-          label="title"
-          placeholder={"Blog Title"}
-        />
-        {errors.title && <p className="error">{errors.title}</p>}
+    <>
+    {showAddNewBlog ? (
+      <Blog onCancel={handleCancel} /> 
+    ) : (
+        <div className="create-blog-cnt">
+          <div className="blog-details-cnt">
+            <BlogInput
+              currentFocus={iscurrentFocus}
+              setcurrentFocus={(label) => setIscurrentFocus(label)}
+              updateValue={(value) =>
+                setNewBlogdata({ ...newBlogdata, title: value })
+              }
+              value={newBlogdata.title}
+              label="title"
+              placeholder={"Blog Title"}
+            />
+            {errors.title && <p className="error">{errors.title}</p>}
 
-        <BlogInput
-          currentFocus={iscurrentFocus}
-          setcurrentFocus={(label) => setIscurrentFocus(label)}
-          updateValue={(value) =>
-            setNewBlogdata({ ...newBlogdata, author: value })
-          }
-          value={newBlogdata.author}
-          label="author"
-          placeholder={"Author Name"}
-        />
-        {errors.author && <p className="error">{errors.author}</p>}
+            <BlogInput
+              currentFocus={iscurrentFocus}
+              setcurrentFocus={(label) => setIscurrentFocus(label)}
+              updateValue={(value) =>
+                setNewBlogdata({ ...newBlogdata, author: value })
+              }
+              value={newBlogdata.author}
+              label="author"
+              placeholder={"Author Name"}
+            />
+            {errors.author && <p className="error">{errors.author}</p>}
 
-        <BlogInput
-          currentFocus={iscurrentFocus}
-          setcurrentFocus={(label) => setIscurrentFocus(label)}
-          updateValue={(value) =>
-            setNewBlogdata({ ...newBlogdata, category: value })
-          }
-          value={newBlogdata.category}
-          label="category"
-          placeholder={"Blog Category"}
-        />
-        {errors.category && <p className="error">{errors.category}</p>}
+            <BlogInput
+              currentFocus={iscurrentFocus}
+              setcurrentFocus={(label) => setIscurrentFocus(label)}
+              updateValue={(value) =>
+                setNewBlogdata({ ...newBlogdata, category: value })
+              }
+              value={newBlogdata.category}
+              label="category"
+              placeholder={"Blog Category"}
+            />
+            {errors.category && <p className="error">{errors.category}</p>}
 
-        <BlogInput
-          currentFocus={iscurrentFocus}
-          setcurrentFocus={(label) => setIscurrentFocus(label)}
-          updateValue={(value) =>
-            setNewBlogdata({ ...newBlogdata, subCategory: value })
-          }
-          value={newBlogdata.subCategory}
-          label="subCategory"
-          placeholder={"Blog Sub Category"}
-        />
-        {errors.subCategory && <p className="error">{errors.subCategory}</p>}
+            <BlogInput
+              currentFocus={iscurrentFocus}
+              setcurrentFocus={(label) => setIscurrentFocus(label)}
+              updateValue={(value) =>
+                setNewBlogdata({ ...newBlogdata, subCategory: value })
+              }
+              value={newBlogdata.subCategory}
+              label="subCategory"
+              placeholder={"Blog Sub Category"}
+            />
+            {errors.subCategory && <p className="error">{errors.subCategory}</p>}
 
-        <BlogInput
-          currentFocus={iscurrentFocus}
-          setcurrentFocus={(label) => setIscurrentFocus(label)}
-          updateValue={(value) =>
-            setNewBlogdata({ ...newBlogdata, tags: value })
-          }
-          value={newBlogdata.tags}
-          label="tags"
-          placeholder={"Tags (separated with a comma)"}
-        />
-        {errors.tags && <p className="error">{errors.tags}</p>}
+            <BlogInput
+              currentFocus={iscurrentFocus}
+              setcurrentFocus={(label) => setIscurrentFocus(label)}
+              updateValue={(value) =>
+                setNewBlogdata({ ...newBlogdata, tags: value })
+              }
+              value={newBlogdata.tags}
+              label="tags"
+              placeholder={"Tags (separated with a comma)"}
+            />
+            {errors.tags && <p className="error">{errors.tags}</p>}
 
-        <div className="status-input-cnt">
-          <p
-            className="input-placeholder"
-            style={{ position: "relative", paddingLeft: 0 }}
-          >
-            Blog status <span style={{ color: "red" }}> *</span>
-          </p>
+            <div className="status-input-cnt">
+              <p
+                className="input-placeholder"
+                style={{ position: "relative", paddingLeft: 0 }}
+              >
+                Blog status <span style={{ color: "red" }}> *</span>
+              </p>
 
-          <div className="select-option-cnt">
-            <label htmlFor="check-active" className="checkbox-cnt">
-              <input
-                type="checkbox"
-                id="check-active"
-                className="checkbox"
-                checked={newBlogdata.status === "active"}
-                onChange={() =>
-                  setNewBlogdata({ ...newBlogdata, status: "active" })
-                }
-              />
-              <p>Active</p>
-            </label>
-            <label htmlFor="check-inactive" className="checkbox-cnt">
-              <input
-                type="checkbox"
-                id="check-inactive"
-                className="checkbox"
-                checked={newBlogdata.status === "notActive"}
-                onChange={() =>
-                  setNewBlogdata({ ...newBlogdata, status: "notActive" })
-                }
-              />
-              <p>In Active</p>
-            </label>
+              <div className="select-option-cnt">
+                <label htmlFor="check-active" className="checkbox-cnt">
+                  <input
+                    type="checkbox"
+                    id="check-active"
+                    className="checkbox"
+                    checked={newBlogdata.status === "active"}
+                    onChange={() =>
+                      setNewBlogdata({ ...newBlogdata, status: "active" })
+                    }
+                  />
+                  <p>Active</p>
+                </label>
+                <label htmlFor="check-inactive" className="checkbox-cnt">
+                  <input
+                    type="checkbox"
+                    id="check-inactive"
+                    className="checkbox"
+                    checked={newBlogdata.status === "notActive"}
+                    onChange={() =>
+                      setNewBlogdata({ ...newBlogdata, status: "notActive" })
+                    }
+                  />
+                  <p>In Active</p>
+                </label>
+              </div>
+              {errors.status && <p className="error">{errors.status}</p>}
+            </div>
           </div>
-          {errors.status && <p className="error">{errors.status}</p>}
+          
+          <div className="text-editor-cnt">
+          {errors.description && <p className="error">{errors.description}</p>}
+            <Editor
+              ref={quillRef}
+              onTextChange={(value) =>
+                setNewBlogdata({ ...newBlogdata, description: value })
+              }
+            />
+
+          </div>
+
+          <div className="blog-long-input-cnt">
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="file-input"
+              onChange={(e) =>
+                setNewBlogdata({ ...newBlogdata, image: e.target.files[0] })
+              }
+            />
+            
+            <p className="absolute-file-name">{newBlogdata?.image?.name}</p>
+            {errors.image && <p className="error">{errors.image}</p>}
+
+            <div
+
+              className="choose-file-label"
+              onClick={() => fileInputRef.current.click()}
+            >
+
+              <p>Choose File</p>
+            </div>
+            <p
+              className="input-placeholder"
+              style={{
+                top: newBlogdata.image ? "-0.8rem" : null,
+              }}
+            >
+              Image
+              <span style={{ color: "red" }}> *</span>
+            </p>
+          </div>
+          <div className="btns-cnt">
+            <div className="action-btn" onClick={handlePublish}>
+              <p>Publish Blog</p>
+            </div>
+            <div
+              className="action-btn"
+              style={{ background: "#3334480D", color: "black" }}
+              onClick={handleAddClick}
+            >
+              <p>Cancel</p>
+            </div>
+          </div>
         </div>
-      </div>
-      
-      <div className="text-editor-cnt">
-      {errors.description && <p className="error">{errors.description}</p>}
-        <Editor
-          ref={quillRef}
-          onTextChange={(value) =>
-            setNewBlogdata({ ...newBlogdata, description: value })
-          }
-        />
-
-      </div>
-
-      <div className="blog-long-input-cnt">
-        <input
-          type="file"
-          ref={fileInputRef}
-          className="file-input"
-          onChange={(e) =>
-            setNewBlogdata({ ...newBlogdata, image: e.target.files[0] })
-          }
-        />
-        
-        <p className="absolute-file-name">{newBlogdata?.image?.name}</p>
-        {errors.image && <p className="error">{errors.image}</p>}
-
-        <div
-
-          className="choose-file-label"
-          onClick={() => fileInputRef.current.click()}
-        >
-
-          <p>Choose File</p>
-        </div>
-        <p
-          className="input-placeholder"
-          style={{
-            top: newBlogdata.image ? "-0.8rem" : null,
-          }}
-        >
-          Image
-          <span style={{ color: "red" }}> *</span>
-        </p>
-      </div>
-      <div className="btns-cnt">
-        <div className="action-btn" onClick={handlePublish}>
-          <p>Publish Blog</p>
-        </div>
-        <div
-          className="action-btn"
-          style={{ background: "#3334480D", color: "black" }}
-        >
-          <p>Cancel</p>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/components/ProfilePage/ProfileItem/DashboardAll/Pages/Blog/Blog.jsx
+++ src/components/ProfilePage/ProfileItem/DashboardAll/Pages/Blog/Blog.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React, { useState } from 'react';
 import "./blogs.css";
 
 import 'quill/dist/quill.core.css';
@@ -13,38 +12,55 @@ import RelatedPost from './widgets/RelatedPost';
 import Tags from './widgets/Tags';
 import MostReads from './widgets/MostReads';
 import Categories from './widgets/Categories';
+import AddNewBlog from './AddBlog/AddNewBlog'; 
 
 const Blog = () => {
+  const [showAddNewBlog, setShowAddNewBlog] = useState(false); 
+
+  const handleAddClick = () => {
+    setShowAddNewBlog(true); 
+  };
+
+  const handleCancel = () => {
+    setShowAddNewBlog(false); 
+  };
+
   return (
-      <div className="dashboard-blogs-container">
-        <h2 className="blogs-title">Blogs</h2>
-        <div className='review-scroll'>
-          <div className="blogs-cnt">
-            <div className="bloglist-cover">
-              <div className="thoughts-cnt">
-                <input
-                  type="text"
-                  className="thoughts-input"
-                  placeholder="Share your thoughts.."
-                />
-                <div className="add-btn">
-                  <img src={AddIcon} alt="icon" className="add-icon" />
+    <>
+      {showAddNewBlog ? (
+        <AddNewBlog onCancel={handleCancel} /> 
+      ) : (
+        <div className="dashboard-blogs-container">
+          <h2 className="blogs-title">Blogs</h2>
+          <div className='review-scroll'>
+            <div className="blogs-cnt">
+              <div className="bloglist-cover">
+                <div className="thoughts-cnt">
+                  <input
+                    type="text"
+                    className="thoughts-input"
+                    placeholder="Share your thoughts.."
+                  />
+                  <div className="add-btn" onClick={handleAddClick}>
+                    <img src={AddIcon} alt="icon" className="add-icon" />
+                  </div>
+                </div>
+                <div className="blog-list-cnt">
+                  <BlogCard/>
                 </div>
               </div>
-              <div className="blog-list-cnt">
-                <BlogCard/>
+              <div className="widget-cnt">
+                <RelatedPost />
+                <Tags/>
+                <MostReads />
+                <Categories/>   
               </div>
             </div>
-            <div className="widget-cnt">
-              <RelatedPost />
-              <Tags/>
-              <MostReads />
-              <Categories/>   
-            </div>
           </div>
-        </div>  
-      </div>
-  )
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Blog
+export default Blog;
